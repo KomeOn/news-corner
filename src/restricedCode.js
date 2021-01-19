3c25752954ffa63da44927dbfe94e8bc2704479d
Sanket Sonowal <sanketsonowal@gmail.com>
2021-01-19 18:39:53 +0530

    minor update Beta version:0.07

--- src/restricedCode.js
+++ src/restricedCode.js
@@ -4,6 +4,7 @@
 */
 //const token = '3ce55404-79bc-4694-9012-a70d3cebf9f6';
 const token = 'a831f6da-e17b-4615-b33c-34a1d50b726c';
+//const token = 'e4efafdb-7fb9-4655-92ce-dba9daae080e';
 
 // format for the respone query
 const format = {
