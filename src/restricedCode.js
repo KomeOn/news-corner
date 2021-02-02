46d4a1930c445ed516948331bb7ce9348db13b8c
Sanket Sonowal <sanketsonowal@gmail.com>
2021-02-02 13:57:11 +0530

    new token

--- src/restricedCode.js
+++ src/restricedCode.js
@@ -5,7 +5,8 @@
 //const token = '3ce55404-79bc-4694-9012-a70d3cebf9f6';
 //const token = 'a831f6da-e17b-4615-b33c-34a1d50b726c';
 //const token = 'e4efafdb-7fb9-4655-92ce-dba9daae080e';
-const token = '83d3375c-5db9-4d90-8755-2de5ce598642';
+//const token = '83d3375c-5db9-4d90-8755-2de5ce598642';
+const token = '28a90cd0-b46e-4dd0-993d-3a8d60a8f1c4';
 
 // format for the respone query
 const format = {
